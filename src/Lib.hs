7dd20840dd4a10cfba47f1828e5a5840035066d1
Alistair Robinson <A.Robinson.8@warwick.ac.uk>
2019-12-01 23:50:39 +0000

    Further improvements

--- src/Lib.hs
+++ src/Lib.hs
@@ -14,20 +14,14 @@ run = do
     hClose file
 
 d1p1 :: String -> String
-d1p1 x = show $ foldl d1p1_process 0 $ map read $ splitOn "\n" x
-
-d1p1_process :: Int -> Int -> Int
-d1p1_process x y = x + d1p1_fuel y
+d1p1 x = show $ foldl (\x y -> x + d1p1_fuel y) 0 $ map read $ splitOn "\n" x
 
 d1p1_fuel :: Int -> Int
 d1p1_fuel x = (x `div` 3) - 2
 
 d1p2 :: String -> String
-d1p2 x = show $ foldl d1p2_process 0 $ map read $ splitOn "\n" x
-
-d1p2_process :: Int -> Int -> Int
-d1p2_process x y = x + d1p2_fuel y
+d1p2 x = show $ foldl (\x y -> x + d1p2_fuel y) 0 $ map read $ splitOn "\n" x
 
 d1p2_fuel :: Int -> Int
-d1p2_fuel x | x > 0 = max (d1p1_fuel x) 0 + d1p2_fuel (d1p1_fuel x)
-            | x < 1 = 0
+d1p2_fuel x | d1p1_fuel x > 0 = d1p1_fuel x + d1p2_fuel (d1p1_fuel x)
+            | otherwise       = 0
