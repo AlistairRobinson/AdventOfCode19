3a77fd1fd3ca7db68d9ba0686cfb19f2a6e44bf5
Alistair Robinson <A.Robinson.8@warwick.ac.uk>
2019-12-01 23:16:57 +0000

    Apply hlint

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,25 +7,25 @@ import Data.List.Split
 run :: IO ()
 run = do
     args <- getArgs
-    file <- openFile (head args) ReadMode 
-    contents <- hGetContents file  
+    file <- openFile (head args) ReadMode
+    contents <- hGetContents file
     putStr (d1p1 contents ++ "\n")
     putStr (d1p2 contents ++ "\n")
-    hClose file 
+    hClose file
 
-d1p1 :: [Char] -> [Char]
+d1p1 :: String -> String
 d1p1 x = show $ foldl d1p1_process 0 $ splitOn "\n" x
 
-d1p1_process :: Int -> [Char] -> Int
-d1p1_process x y = x + (floor ((read y) / 3)) - 2
+d1p1_process :: Int -> String -> Int
+d1p1_process x y = x + floor (read y / 3) - 2
 
-d1p2 :: [Char] -> [Char]
+d1p2 :: String -> String
 d1p2 x = show $ foldl d1p2_process 0 $ splitOn "\n" x
 
-d1p2_process :: Int -> [Char] -> Int
+d1p2_process :: Int -> String -> Int
 d1p2_process x y = x + d1p2_fuel (read y)
 
 d1p2_fuel :: Int -> Int
-d1p2_fuel x | x > 0 = max (f x) (0) + d1p2_fuel (f x) 
+d1p2_fuel x | x > 0 = max (f x) 0 + d1p2_fuel (f x)
             | x < 1 = 0
-              where f x = (floor ((fromIntegral x) / 3)) - 2
+              where f x = floor ((fromIntegral x) / 3) - 2
