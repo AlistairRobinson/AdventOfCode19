99a7d900332559702f67e3edc22a4b754edb64b2
Alistair Robinson <A.Robinson.8@warwick.ac.uk>
2019-12-01 20:31:47 +0000

    Complete day 1 part 1

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,10 +1,19 @@
-module Lib
-    ( run
-    ) where
+module Lib where
 
 import System.Environment
+import System.IO
+import Data.List.Split
 
 run :: IO ()
 run = do
     args <- getArgs
-    putStrLn (head args)
+    file <- openFile (head args) ReadMode 
+    contents <- hGetContents file  
+    putStr (d1p1 contents ++ "\n")
+    hClose file 
+
+d1p1 :: [Char] -> [Char]
+d1p1 x = show $ foldl d1p1_process 0 $ splitOn "\n" x
+
+d1p1_process :: Int -> [Char] -> Int
+d1p1_process x y = x + (floor ((read y) / 3)) - 2
