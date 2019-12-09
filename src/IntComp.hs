e1932295a67df66130985c34f6ce1ce786fa3bf6
Alistair Robinson <A.Robinson.8@warwick.ac.uk>
2019-12-09 13:46:36 +0000

    Expant IntComp to include relative addressing

--- src/IntComp.hs
+++ src/IntComp.hs
@@ -13,6 +13,7 @@ data Instr = Instr {
 
 data Program = Program {
     pc   :: Int,
+    rel  :: Int,
     mem  :: [Int],
     args :: [Int]
 } deriving Show
@@ -21,6 +22,8 @@ data Param = Addr {
     pos :: Int
 } | Val {
     val :: Int
+} | Rel {
+    pos :: Int 
 } | None deriving Show
 
 data Result = Memory {
@@ -29,27 +32,27 @@ data Result = Memory {
     inp   :: [Int]
 } | Jump {
     loc   :: Int
+} | Shift {
+    loc   :: Int
 }
 
-data Op = Add | Mult | In | Out | Stop | BrT | BrF | Lt | Eq deriving Show
+data Op = Add | Mult | In | Out | Stop | BrT | BrF | Lt | Eq | Mov deriving Show
 
 next = Prelude.drop
 set i v xs = take i xs ++ (v: next (i + 1) xs)
 
 parse :: Int -> [Int] -> Instr
 parse i []           = Instr Stop None None None
-parse i (x:c:b:a:xs) = Instr (op x)
-                        (if x `mod` 1000  >= 100   then Val c else Addr c)
-                        (if x `mod` 10000 >= 1000  then Val b else Addr b)
-                        (if x             >= 10000 then Val a else Addr a)
-parse i (x:c:b:xs)   = Instr (op x)
-                        (if x `mod` 1000  >= 100   then Val c else Addr c)
-                        (if x `mod` 10000 >= 1000  then Val b else Addr b)
-                        None
-parse i (x:c:xs)     = Instr (op x)
-                        (if x `mod` 1000  >= 100   then Val c else Addr c)
-                        None None
-parse i (x:xs)       = Instr (op x) None None None
+parse i (x:c:b:a:xs) = Instr (op x) (param x 1 c) (param x 10 b) (param x 100 a)
+parse i (x:c:b:xs)   = Instr (op x) (param x 1 c) (param x 10 b)  None
+parse i (x:c:xs)     = Instr (op x) (param x 1 c)  None           None
+parse i (x:xs)       = Instr (op x)  None          None           None
+
+param :: Int -> Int -> Int -> Param
+param x y a = case (x `mod` (y * 1000)) `div` (y * 100) of
+                  2 -> Rel  a
+                  1 -> Val  a
+                  _ -> Addr a
 
 op :: Int -> Op
 op x = case x `mod` 100 of
@@ -61,6 +64,7 @@ op x = case x `mod` 100 of
          6  -> BrF
          7  -> Lt
          8  -> Eq
+         9  -> Rel
          99 -> Stop
 
 op_length :: Op -> Int
@@ -72,52 +76,57 @@ op_length BrT  = 3
 op_length BrF  = 3
 op_length In   = 2
 op_length Out  = 2
+op_length Rel  = 2
 op_length Stop = 1
 
-run_ic :: Int -> [Int] -> [Int] -> [Int]
-run_ic n mem a = case res of
-                   Memory [] r as -> r
-                   Memory m  r as -> r ++ (run_ic (n + op_length (o ins)) m as)
-                   Jump   l       -> run_ic l mem a
-                   where ins = parse n (drop n mem)
-                         res = execute ins mem a
+run_ic :: Int -> Int -> [Int] -> [Int] -> [Int]
+run_ic n q mem a = case res of
+                     Memory [] r as -> r
+                     Memory m  r as -> r ++ (run_ic (n + op_length (o ins)) q m as)
+                     Jump   l       -> run_ic l q mem a
+                     Shift  l       -> run_ic (n + op_length (o ins)) l m as
+                     where ins = parse n (drop n mem)
+                           res = execute ins q mem a
 
 run_prog :: Program -> [Int]
-run_prog (Program n mem a) = run_ic n mem a
+run_prog (Program n q mem a) = run_ic n mem a
 
 yield_prog :: Program -> (Program, Int)
-yield_prog (Program n mem a) = case res of
-            Memory [] r  as -> (Program n [] as, 0)
-            Memory m  [] as -> yield_prog (Program (n + op_length (o ins)) m as)
-            Memory m  r  as -> (Program (n + op_length (o ins)) m as, head r)
-            Jump   l        -> yield_prog (Program l mem a)
-            where ins = parse n (drop n mem)
-                  res = execute ins mem a
+yield_prog (Program n q mem a) = case res of
+        Memory [] r  as -> (Program n q [] as, 0)
+        Memory m  [] as -> yield_prog (Program (n + op_length (o ins)) q m as)
+        Memory m  r  as -> (Program (n + op_length (o ins)) q m as, head r)
+        Jump   l        -> yield_prog (Program l q mem a)
+        Shift  l        -> yield_prog (Program (n + op_length (o ins)) l m as)
+        where ins = parse n (drop n mem)
+              res = execute ins q mem a
 
-execute :: Instr -> [Int] -> [Int] -> Result
-execute (Instr Stop       _  _ _) r i =
+execute :: Instr -> Int -> [Int] -> [Int] -> Result
+execute (Instr Stop       _  _ _) q r i =
     Memory [] [] i
-execute (Instr Add  c b (Addr a)) r i =
-    Memory (set a (eval b r + eval c r) r) [] i
-execute (Instr Mult c b (Addr a)) r i =
-    Memory (set a (eval b r * eval c r) r) [] i
-execute (Instr In   (Addr c) b a) r i =
+execute (Instr Add  c b (Addr a)) q r i =
+    Memory (set a (eval b q r + eval c q r) r) [] i
+execute (Instr Mult c b (Addr a)) q r i =
+    Memory (set a (eval b q r * eval c q r) r) [] i
+execute (Instr In   (Addr c) b a) q r i =
     Memory (set c (head i) r) [] (tail i)
-execute (Instr Out         c b a) r i =
-    Memory r [eval c r] i
-execute (Instr BrT         c b a) r i
-    | eval c r /= 0                   = Jump (eval b r)
-    | otherwise                       = Memory r [] i
-execute (Instr BrF         c b a) r i
-    | eval c r == 0                   = Jump (eval b r)
-    | otherwise                       = Memory r [] i
-execute (Instr Lt   c b (Addr a)) r i
-    | eval c r < eval b r             = Memory (set a 1 r) [] i
-    | otherwise                       = Memory (set a 0 r) [] i
-execute (Instr Eq   c b (Addr a)) r i
-    | eval c r == eval b r            = Memory (set a 1 r) [] i
-    | otherwise                       = Memory (set a 0 r) [] i
+execute (Instr Out         c b a) q r i =
+    Memory r [eval c q r] i
+execute (Instr BrT         c b a) q r i
+    | eval c q r /= 0                   = Jump (eval b q r)
+    | otherwise                         = Memory r [] i
+execute (Instr BrF         c b a) q r i
+    | eval c q r == 0                   = Jump (eval b q r)
+    | otherwise                         = Memory r [] i
+execute (Instr Lt   c b (Addr a)) q r i
+    | eval c q r < eval b q r           = Memory (set a 1 r) [] i
+    | otherwise                         = Memory (set a 0 r) [] i
+execute (Instr Eq   c b (Addr a)) q r i
+    | eval c q r == eval b q r          = Memory (set a 1 r) [] i
+    | otherwise                         = Memory (set a 0 r) [] i
+execute (Instr Mov         c b a) q r i = Shift (q + (eval c q r))
 
-eval :: Param -> [Int] -> Int
-eval (Addr  a) ys = ys!!a
-eval (Val v)   ys = v
+eval :: Param -> Int -> [Int] -> Int
+eval (Addr  a) q ys = ys!!a
+eval (Val   v) q ys = v
+eval (Rel   s) q ys = ys!!(q + s) 
